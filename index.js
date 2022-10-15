960b6c76d08c840dbdc6bbeca69432292ad0e8fb
Zaytsevie <115174587+Zaytsevie@users.noreply.github.com>
2022-10-15 19:31:10 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+// number --> string
+let age = 37
+age = "37"
+alert(age)
+
+// string --> number
+let myHeight = "182"
+myHeight = 182
+alert(myHeight)
+
+// boolean ---> null
+let russian = true
+russian = null
+alert(russian)
+
